0eadcb891281042af59a546b9f1cd2269b991661
ahnaf005 <ahnaffaisalalavi@gmail.com>
2018-04-27 18:27:09 +0600

    branch commit

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class JavaApplication3 {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
+        String s=input.next();
+        System.out.println(s);
         
     }
     
