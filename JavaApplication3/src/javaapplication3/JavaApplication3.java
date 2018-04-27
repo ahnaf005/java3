835ceef56b677f36cae7829305a45c275a9d6a15
ahnaf005 <ahnaffaisalalavi@gmail.com>
2018-04-27 18:05:04 +0600

    first commit

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,11 @@
+package javaapplication3;
+
+import java.util.Scanner;
+
+public class JavaApplication3 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        
+    }
+    
+}
